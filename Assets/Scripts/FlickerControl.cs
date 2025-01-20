3cc08cde2d0cf02a348d246a0dbf8324ba5820f5
Playstarstudio <charles@playstarstudio.com>
2025-01-20 02:33:47 -0500

    Textures Add, light settings adjusted

--- Assets/Scripts/FlickerControl.cs
+++ Assets/Scripts/FlickerControl.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class FlickerControl : MonoBehaviour
 {
     public bool isFlickering = false;
     public float timeDelay;
+    public float lightMin;
+    public float lightMax;
     void Update()
     {
         if (isFlickering == false)
@@ -20,10 +23,10 @@ public class FlickerControl : MonoBehaviour
     IEnumerator FlickeringLight()
     {
         isFlickering = true;
-        this.gameObject.GetComponent<Light>().intensity = Random.Range(1f, 2.5f);
+        this.gameObject.GetComponent<Light>().intensity = Random.Range(lightMin, lightMax);
         timeDelay = Random.Range(0.01f, 0.1f);
         yield return new WaitForSeconds(timeDelay);
-        this.gameObject.GetComponent<Light>().intensity = Random.Range(1f, 2.5f);
+        this.gameObject.GetComponent<Light>().intensity = Random.Range(lightMin, lightMax);
         timeDelay = Random.Range(0.01f, 0.1f);
         yield return new WaitForSeconds(timeDelay);
         isFlickering = false;
