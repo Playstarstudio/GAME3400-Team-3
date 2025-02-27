498e61f0b5d4299e81e2c99456ba2a7507bd226c
Aidan Moreau <moreau.ai@northeastern.edu>
2025-02-26 21:44:18 -0500

    fixed movement

--- Assets/ResetTerrain.cs
+++ Assets/ResetTerrain.cs
@@ -2,16 +2,13 @@ using UnityEngine;
 
 public class ResetTerrain : MonoBehaviour
 {
-    public GameObject terrain;
+    public Transform terrain;
 
-    private Vector3 resetPosition;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        resetPosition = terrain.transform.position;
-    }
+
+  
 
     private void OnTriggerEnter(Collider other) {
-        terrain.transform.position = resetPosition;
+        if(other.CompareTag("Player"))
+        terrain.position = new Vector3(terrain.position.x, terrain.position.y, terrain.position.z + 500);
     }
 }
