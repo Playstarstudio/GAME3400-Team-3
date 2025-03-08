6e990ff8457dc0dd479ed07e7a1b15b38bb6d5ec
Aidan Moreau <moreau.ai@northeastern.edu>
2025-03-07 23:50:56 -0500

    Added Shine sound to project, finalized pick up script, created roommanager

--- Assets/PickUpGem.cs
+++ Assets/PickUpGem.cs
@@ -5,39 +5,54 @@ public class PickUpGem : MonoBehaviour
 {
     public GameObject uiObject;
     [SerializeField] private GameObject completedStaff;
-    [SerializeField] private RoomManager roomManager;
+    [SerializeField] private RoomManager7 roomManager;
     [SerializeField] private GameObject playerStaff;
     private Transform staffTransform;
+
+    private bool playerInRange;
+
+    [SerializeField] private GameObject gemstone;
+    [SerializeField] private GameObject trigger;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         staffTransform = playerStaff.transform;
+        playerInRange = false;
+        uiObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            SwapStaffs(playerStaff, completedStaff);
+            Destroy(gemstone);
+            Destroy(uiObject);
+            //Disables script
+            Destroy(trigger);
+        }
+
     }
 
     void OnTriggerEnter(Collider collider) {
         if(collider.CompareTag("Player")) {
+            playerInRange = true;
             uiObject.SetActive(true);
-            if(Input.GetKeyDown(KeyCode.E)) {
-                SwapStaffs(playerStaff, completedStaff);
-            }
         }
     }
 
     void OnTriggerExit(Collider collider) {
         if(collider.CompareTag("Player")) {
+            playerInRange = false;
             uiObject.SetActive(false);
         }
     }
 
     void SwapStaffs(GameObject playerStaff, GameObject fullStaff) {
     GameObject completeStaff = Instantiate(fullStaff, playerStaff.transform.position, playerStaff.transform.rotation);
-    Destroy(playerStaff);
+    completeStaff.transform.SetParent(playerStaff.transform.parent);
+    DestroyImmediate(playerStaff, true);
     }
 }
