cd4082e7d2368cf62c3388da943a691ed5fba700
Aidan Moreau <moreau.ai@northeastern.edu>
2025-04-17 15:53:15 -0400

    Pick up suit script

---
+++ Assets/Scripts/PickUpSuit.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PickUpSuit : MonoBehaviour
+{
+    public GameObject uiObject;
+    public GameObject suit;
+    public P_Manager playerManager;
+    private bool playerInRange;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
