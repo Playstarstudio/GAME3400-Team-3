e7530d28927a9c5e05b51e1eae8e6b53e0c18cb2
Aidan Moreau <moreau.ai@northeastern.edu>
2025-04-17 16:13:46 -0400

    Scripting polish

--- Assets/Scripts/OpenLocker.cs
+++ Assets/Scripts/OpenLocker.cs
@@ -6,18 +6,34 @@ public class OpenLocker : MonoBehaviour
     public GameObject uiObject;
     public GameObject suitTrigger;
     bool isOpen = false;
+    bool playerInRange = false;
+
+    private void Start() {
+        uiObject.SetActive(false);
+        suitTrigger.SetActive(false);
+    }
+
+    void Update() {
+        if(playerInRange && Input.GetKeyDown(KeyCode.E)) {
+            isOpen = true;
+            lockerDoor.transform.Rotate(0, 90, 0);
+            suitTrigger.SetActive(true);
+        }
+        if(isOpen) {
+            Destroy(uiObject);
+            Destroy(this.gameObject);
+        }
+    }
     
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player")) {
             uiObject.SetActive(true);
-            if(!isOpen) {
-                lockerDoor.transform.Rotate(0, 90, 0);
-                isOpen = true;
-            }
+            playerInRange = true;            
         }
     }
     private void OnTriggerExit(Collider other) {
         if(other.CompareTag("Player")) {
+            playerInRange = false;
             uiObject.SetActive(false);
         }
     }
